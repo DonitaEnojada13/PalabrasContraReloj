51ae5cf242164e6b6e8d906e009544c35a95e2b1
DonitaEnojada13 <donitaprogramadora@gmail.com>
2026-05-18 22:06:49 -0600

    Pequena clase de lectorDiccionario para no olvidarla

---
+++ LectorDiccionario.java
@@ -0,0 +1,10 @@
+public class LectorDiccionario {
+
+    /**
+     * Clase encargada de leer el diccionario
+     * y, posteriormente, agregarla a una estructura de datos
+     * auxiliar (yo creo que seria un mapa hash o algo asi), a
+     * ver si me sale esto, sigo sin entender a ciencia cierta
+     * lo que son lo buffer reader y esas cosas
+     */
+}
