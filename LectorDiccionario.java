188d263f477303b00bd876cdbaab4746cadba0b9
DonitaEnojada13 <donitaprogramadora@gmail.com>
2026-05-19 22:22:56 -0600

    Cambio pequeno

--- LectorDiccionario.java
+++ LectorDiccionario.java
@@ -7,5 +7,12 @@ public class LectorDiccionario {
      * ver si me sale esto, sigo sin entender a ciencia cierta
      * lo que son lo buffer reader y esas cosas
      */
-    
+
+    private boolean palabraValida(Strign s) {
+	if (s == null)
+	    throw new IllegalArgumentException("Un string vacio, que brujeria es esta");
+	if (s.isEmpty() || s.length > 9)
+	    return false;
+	
+    }
 }
