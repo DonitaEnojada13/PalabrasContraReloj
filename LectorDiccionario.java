6f76682e77b215a1c8b0cd659c00289ac4dea063
A4DREK <yael.adrian640305@gmail.com>
2026-05-20 11:12:05 -0600

    En la clase Mapa Hash hice cambios para que no tome errores con el Arbol, agregué lo de Comparable

--- LectorDiccionario.java
+++ LectorDiccionario.java
@@ -8,10 +8,10 @@ public class LectorDiccionario {
      * lo que son lo buffer reader y esas cosas
      */
 
-    private boolean palabraValida(Strign s) {
+    private boolean palabraValida(String s) {
 	if (s == null)
 	    throw new IllegalArgumentException("Un string vacio, que brujeria es esta");
-	if (s.isEmpty() || s.length > 9)
+	if (s.isEmpty() || s.length() > 9)
 	    return false;
 	
     }
--- MapaHash.java
+++ MapaHash.java
@@ -1,14 +1,24 @@
-public class MapaHash<K, V> {
+public class MapaHash<K extends Comparable<K>, V> {
     //expresiones regulares
 
-    private class Entrada {
-	public K llave;
-	public V valor;
+    private class Entrada implements Comparable<Entrada>{
+		private K llave;
+		private	 V valor;
 
-	public Entrada(K llave, V valor, boolean estado) {
-	    this.llave = llave;
-	    this.valor = valor;
-	}
+		public Entrada(K llave, V valor, boolean estado) {
+	    	this.llave = llave;	
+	    	this.valor = valor;
+		}
+
+		@Override
+		public int compareTo(Entrada otraEntrada){
+			return this.llave.compareTo(otraEntrada.llave);
+		}
+
+		@Override
+		public String toString(){
+			return "{ " + llave + ": " + valor + " }";
+		}
     }
 
     private ArbolAVL<Entrada>[] entradas;
